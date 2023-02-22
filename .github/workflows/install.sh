6fe680a8ef82efd46a87004437eafbea5977991f
Fernando Alemán <fernandoaleman@mac.com>
2023-02-22 12:27:06 -0600

    Test wkhtmltopdf

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -4,10 +4,20 @@ echo "# ----------------------------------------------------------"
 echo "# Install script"
 echo "# ----------------------------------------------------------"
 sudo apt update
-sudo apt install -y pandoc
+sudo apt-get install -y \
+  build-essential \
+  libcurl4-openssl-dev \
+  libxml2-utils \
+  curl \
+  xz-utils \
+  libfontconfig \
+  zlib1g libfreetype6 \
+  libxrender1 \
+  libxext6 \
+  libx11-6
+curl -L -o wkhtmltopdf.tar.xz https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz &&
+tar -xf wkhtmltopdf.tar.xz &&
+sudo mv wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf &&
+sudo chmod +x /usr/local/bin/wkhtmltopdf
 echo "# *** END: Install script"
-echo "# ----------------------------------------------------------"
-echo "# Create the hello.txt file"
-echo "# ----------------------------------------------------------"
-echo "Hello World!" > hello.txt
 echo "# ----------------------------------------------------------"
